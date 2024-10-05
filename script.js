71b1af3d9d282dba6288a3151dcb65db1ce458e5
sakibMERN <sakib.sr57@gmail.com>
2024-10-05 10:49:19 +0600

    latest post section making dynamic

--- script.js
+++ script.js
@@ -18,6 +18,61 @@ const loadPosts = async (category) => {
   displayAllPost(data.posts);
 };
 
+
+//Latest post 
+const loadLatestPost = async () => {
+  const uri = "https://openapi.programming-hero.com/api/retro-forum/latest-posts";
+  
+    const res = await fetch(uri);
+    const data = await res.json();
+    displayLatestPost(data);
+  
+}
+
+const displayLatestPost = (posts) => {
+  posts.forEach(post => {
+    const latestPostContainer = document.getElementById("latest-post-container");
+    console.log("hi post", post);
+    //create new card for showing latest post
+    const div = document.createElement("div")
+    div.classList.add("card", "lg:w-96", "pb-5", "bg-base-100", "shadow-2xl");
+    div.innerHTML = `
+
+          <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+              <img
+                  src="${post.cover_image
+                  }"
+                  alt="Shoes"
+                  class="rounded-xl"
+              />
+          </figure>
+          <div class="p-5 lg:p-10 space-y-4 lg:space-y-5">
+              <p class="opacity-50 text-start">
+                  <i class="fa-solid fa-calendar-days me-2"></i>${post.author.posted_date ? post.author.posted_date : "No publish date"}
+              </p>
+              <h2 class="card-title text-start">${post.title}</h2>
+              <p class="text-start">
+                  ${post.description}
+              </p>
+              <div class="card-actions flex gap-5 items-center">
+                  <div class="avatar">
+                      <div
+                          class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+                      >
+                          <img
+                          src="${post.profile_image}"
+                          />
+                      </div>
+                  </div>
+              <div>
+              <h3 class="text-start font-extrabold">${post.author.name}</h3>
+              <p class="text-start opacity-60">${post.author.designation ? post.author.designation : "Unknown"}</p>
+          </div>
+    `
+    latestPostContainer.appendChild(div);
+  })
+}
+
 const displayAllPost = (posts) => {
   const postContainer = document.getElementById("post-container");
   postContainer.innerHTML = "";
@@ -82,6 +137,7 @@ const displayAllPost = (posts) => {
 };
 
 loadPosts();
+loadLatestPost();
 
 const handleSearchByCategory = () => {
   const searchText = document.getElementById("searchPosts").value;
